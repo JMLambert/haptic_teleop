42fe8e86fb3761d0c4e466cc077c04fddc870073
JMLambert <michel.lambert@cnrc-nrc.gc.ca>
2014-04-24 14:01:11 -0400

    first commit

---
+++ comau_config/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/opt/ros/groovy/include;/usr/include/eigen3".split(';') if "/opt/ros/groovy/include;/usr/include/eigen3" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;moveit_core;moveit_ros_planning_interface".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "comau_config"
+PROJECT_SPACE_DIR = "/home/jml/catkin_ws/src/comau_config/build/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ comau_config/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/jml/catkin_ws/src/comau_config/build/gtest
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
